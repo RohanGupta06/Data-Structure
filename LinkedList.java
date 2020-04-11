7595345fb3d69151784863768579ba9ab7ccfc9c
Rohan gupta <rohange@outlook.com>
2020-04-11 21:12:15 +0530

    updated with add functions

--- LinkedList.java
+++ LinkedList.java
@@ -25,15 +25,39 @@ class LinkedList
 		n.Link = head;
 		head = n;
 	}
+	public void insertAfter(Node prev_node, int new_data)
+	{
+		if(prev_node == null)
+		{
+			System.out.println("You did something wrong");
+		}
+		Node new_node = new Node(new_data);
+		new_node.Link = prev_node.Link;
+		prev_node.Link = new_node;
+	}
+	public void append(int new_data)
+	{
+		Node new_node = new Node(new_data);
+		if(head == null)
+		{
+			head = new Node(new_data);
+		}
+		Node temp = head;
+		while(temp.Link!=null)
+		{
+			temp = temp.Link;
+		}
+		temp.Link = new_node;
+	}
+			
 	public static void main(String args[])
 	{
 		LinkedList obj1 = new LinkedList();
-		obj1.head = new Node(1);
-		Node second = new Node(3);
-		Node third = new Node(5);
-		obj1.head.Link=second;
-		second.Link=third;
-		obj1.pushInFront(0);
+		obj1.append(1);
+		obj1.pushInFront(3);
+		obj1.append(5);
+		obj1.insertAfter(obj1.head.Link.Link,5);
+		System.out.println("Linked List");
 		obj1.PrintList();
 	}
 }
